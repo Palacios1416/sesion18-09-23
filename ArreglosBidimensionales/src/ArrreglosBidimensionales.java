7adbfd17fa26427b0760f7e6ac584601fac24046
Dereck Palacios <dpalaciosurbina@gmail.com>
2023-09-19 17:58:56 -0600

    terminado

--- ArreglosBidimensionales/src/ArrreglosBidimensionales.java
+++ ArreglosBidimensionales/src/ArrreglosBidimensionales.java
@@ -6,20 +6,25 @@ public class ArrreglosBidimensionales {
     
     int[][] matrizB = {{1,2}, {3,4}};
 
-    System.out.println("Ingrese la cantidad de filas: ");
+    System.out.print("Ingrese la cantidad de filas: ");
         int cantFilas = scanner.nextInt();
-    System.out.println("Ingrese la cantidad de columnas: ");
+    System.out.print("Ingrese la cantidad de columnas: ");
         int cantColumnas = scanner.nextInt();
     int[][] matrizA = new int[cantFilas][cantColumnas];
-        System.out.println("Ingrese los elemntos de la matriz: ");
+        System.out.println("Ingrese los elementos de la matriz: ");
         for (int i = 0; i < cantFilas; i++) {
             for (int j = 0; j < cantColumnas; j++) {
-                System.out.print("Arreglo A [" + i + "," + j + "]");
-
+                System.out.print("Arreglo A [" + i + "," + j + "] = ");
+                matrizA[i][j] = scanner.nextInt();
             }
         }
-
-
+        System.out.println("Los datos en forma de matriz: ");
+        for (int i = 0; i < matrizA.length; i++) {
+            for (int j = 0; j < matrizA.length; j++) {
+                System.out.print(matrizA[i][j] + "\t");
+            }
+            System.out.println();
+        }
 
 
 
