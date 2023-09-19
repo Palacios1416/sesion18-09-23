e5819dbf1b101cb13efd1cf3134d6832142d82f0
Dereck Palacios <dpalaciosurbina@gmail.com>
2023-09-19 17:14:03 -0600

    terminado

--- ArreglosBidimensionales/SumaArreglos.java
+++ ArreglosBidimensionales/SumaArreglos.java
@@ -5,6 +5,13 @@ public class SumaArreglos {
 
         System.out.println("Tamaño de los arreglos");
         int tamañoMatriz = scanner.nextInt();
+
+        while (tamañoMatriz <= 0) {
+            System.out.println("el tamaño no puede ser menor o igual a cero");
+            System.out.println("introduzca el tamaño otra vez");
+            tamañoMatriz = scanner.nextInt();
+        }
+
         int [] arreglo1 = new int[tamañoMatriz];
         int [] arreglo2 = new int[tamañoMatriz];
         int [] arreglosSuma = new int[tamañoMatriz];
@@ -13,16 +20,33 @@ public class SumaArreglos {
         for (int i = 0; i < tamañoMatriz; i++) {
             System.out.println("Arreglo 1 [ " + (i+1) + " ]");
             arreglo1[i] = scanner.nextInt();
+            
         }
+        System.out.println("ingrese los valores del arreglo 2");
         for (int i = 0; i < tamañoMatriz; i++) {
             System.out.println("Arreglo 2 [ " + (i+1) + " ]");
             arreglo2[i] = scanner.nextInt();
+            
         }
+        System.out.print("Arreglo 1 = [");
+        for (int i = 0; i < tamañoMatriz; i++) {
+            System.out.print(arreglo1[i] + ",");
+        }
+        System.out.print("]\n");
+
+        System.out.print("Arreglo 2 = [");
+        for (int i = 0; i < tamañoMatriz; i++) {
+            System.out.print(arreglo2[i] + ",");
+        }
+        System.out.println("]\n");
 
         System.out.println("La suma de arreglo 1 y arreglo 2 es: ");
+        System.out.print("[ ");
         for (int i = 0; i < tamañoMatriz; i++) {
-            System.out.println("Arreglo 2 [ " + (i+1) + " ]");
-            arreglo2[i] = scanner.nextInt();
+            arreglosSuma[i] = arreglo1[i] + arreglo2[i];
+             System.out.print(arreglosSuma[i] + ", ");
+            
         }
+        System.out.print("]");
     }
 }
